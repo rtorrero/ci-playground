1ed5cfd72a7033302bcbd0177b83ed3058259368
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-08-30 15:45:52 +0100

    wip

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example/hello
 go 1.19
 
 require (
+	github.com/rtorrero/ci-playground-beta v0.0.3 // indirect
 	moul.io/banner v1.0.1 // indirect
 	moul.io/motd v1.0.0 // indirect
 )
--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"moul.io/motd"
+	playground "github.com/rtorrero/ci-playground-beta/greetings"
 )
 
 func main() {
 	fmt.Print(motd.Default())
+	fmt.Print(playground.Hello("world"))
 }
